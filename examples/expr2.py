9fa56a0bb72624400c9a5632e30d7b0db5708492
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-02-04 22:01:22 +0530

    tmp

--- examples/expr2.py
+++ examples/expr2.py
@@ -1,10 +1,16 @@
+def print_int(x: i32):
+    if x < 0:
+        print("-", end="")
+    if x == 0:
+        print(x)
+    while x > 0:
+        print(x % 10)
+        x /= 10
+
+
 def main0():
     x: i32
     x = (2+3)*5
     print(x)
 
 main0()
-
-# Not implemented yet in LPython:
-#if __name__ == "__main__":
-#    main()
