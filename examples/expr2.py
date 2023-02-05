32231e27ee6ab6f4b4b5c6b874cd92209f50e57e
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-02-05 15:48:10 +0530

    tmp

--- examples/expr2.py
+++ examples/expr2.py
@@ -1,16 +1,28 @@
+from ltypes import i32
+
 def print_int(x: i32):
-    if x < 0:
-        print("-", end="")
     if x == 0:
         print(x)
+    elif x < 0:
+        print("-")
+        x *= -1
+    digits_cnt: i32 = 0
     while x > 0:
-        print(x % 10)
-        x /= 10
-
+        # push x % 10 onto stack
+        digits_cnt += 1
+        x = i32(x / 10)
+    while digits_cnt > 0:
+        # print digit on stack top
+        digits_cnt -= 1
+    print()
 
 def main0():
-    x: i32
-    x = (2+3)*5
-    print(x)
+    print_int(0)
+    print_int(-1)
+    print_int(1)
+    print_int(-23)
+    print_int(23)
+    print_int(42523458)
+    print_int(-42523458)
 
 main0()
