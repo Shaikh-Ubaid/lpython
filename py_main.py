72160096bc74e0709c1a3426203221ed577cbd11
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-05-13 18:18:23 +0530

    tmp

--- py_main.py
+++ py_main.py
@@ -2,9 +2,9 @@ import sysconfig
 from distutils.sysconfig import get_python_inc
 print(sysconfig.get_makefile_filename())
 print("="*80)
-print(sysconfig.get_config_var('CFLAGS'))
+print(sysconfig.get_config_var('PY_CFLAGS'))
 print("="*80)
-print(sysconfig.get_config_var('LDFLAGS'))
+print(sysconfig.get_config_var('PY_LDFLAGS'))
 print("="*80)
 print(sysconfig.get_config_var('LIBS'))
 print("="*80)
